12f4e87f716d8f1c1b2df7fd7b43dcb48800a7f0
wiaoj <bertandeniz7@gmail.com>
2022-09-21 00:03:32 +0300

    PhoneNumber Delete command added

--- Application/Handlers/Phonenumbers/Commands/Update/UpdatePhoneNumberCommand.cs
+++ Application/Handlers/Phonenumbers/Commands/Update/UpdatePhoneNumberCommand.cs
@@ -1,14 +1,40 @@
-﻿using Application.Handlers.Phonenumbers.Dtos.Commands;
+﻿using Application.Handlers.Phonenumbers.BusinessRules;
+using Application.Handlers.Phonenumbers.Dtos.Commands;
+using Application.Handlers.PhoneNumbers.Constants;
+using Application.Repositories;
+using AutoMapper;
+using Domain.Entities;
 using MediatR;
 
 namespace Application.Handlers.Phonenumbers.Commands.Update;
 
 public class UpdatePhoneNumberCommand : IRequest<UpdatedPhoneNumberDto> {
 	public Guid Id { get; set; }
+	public Guid UserId { get; set; }
+    public String CountryCode { get; set; }
+    public String Number { get; set; }
 
-	internal class UpdateCommandHandler : IRequestHandler<UpdatePhoneNumberCommand, UpdatedPhoneNumberDto> {
-		public Task<UpdatedPhoneNumberDto> Handle(UpdatePhoneNumberCommand request, CancellationToken cancellationToken) {
-			throw new NotImplementedException();
+    internal class UpdateCommandHandler : IRequestHandler<UpdatePhoneNumberCommand, UpdatedPhoneNumberDto> {
+		private readonly IPhoneNumberRepository _phoneNumberRepository;
+		private readonly PhoneNumberBusinessRules _phoneNumberBusinessRules;
+		private readonly IMapper _mapper;
+
+		public UpdateCommandHandler(IPhoneNumberRepository phoneNumberRepository, PhoneNumberBusinessRules phoneNumberBusinessRules, IMapper mapper) {
+			_phoneNumberRepository = phoneNumberRepository;
+			_phoneNumberBusinessRules = phoneNumberBusinessRules;
+			_mapper = mapper;
+		}
+
+		public async Task<UpdatedPhoneNumberDto> Handle(UpdatePhoneNumberCommand request, CancellationToken cancellationToken) {
+			await _phoneNumberBusinessRules.PhoneNumberShouldExistWhenRequestId(request.Id);
+
+			PhoneNumber mappedPhoneNumber = _mapper.Map<PhoneNumber>(request);
+			PhoneNumber updatedPhoneNumber = await _phoneNumberRepository.UpdateAsync(mappedPhoneNumber);
+
+			UpdatedPhoneNumberDto updatedPhoneNumberDto = _mapper.Map<UpdatedPhoneNumberDto>(updatedPhoneNumber);
+			updatedPhoneNumberDto.Message = PhoneNumberMessageConstants.Updated;
+
+			return updatedPhoneNumberDto;
 		}
 	}
 }
--- Application/Handlers/Phonenumbers/Dtos/Commands/UpdatedPhoneNumberDto.cs
+++ Application/Handlers/Phonenumbers/Dtos/Commands/UpdatedPhoneNumberDto.cs
@@ -1,5 +1,8 @@
 ﻿namespace Application.Handlers.Phonenumbers.Dtos.Commands;
 public record UpdatedPhoneNumberDto {
     public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public String CountryCode { get; set; }
+    public String Number { get; set; }
     public String Message { get; set; }
 }
