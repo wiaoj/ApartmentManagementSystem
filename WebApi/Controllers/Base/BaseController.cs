2504012c5b7392c40f69056dcd4b0fe919ad9594
wiaoj <bertandeniz7@gmail.com>
2022-09-21 00:27:18 +0300

    PhoneNumber Controller (add-delete-update) added

---
+++ WebApi/Controllers/Base/BaseController.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers.Base;
+
+public class BaseController : ControllerBase {
+    protected IMediator? Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    private IMediator? _mediator;
+}
---
+++ WebApi/Controllers/PhoneNumbersController.cs
@@ -0,0 +1,33 @@
+﻿using Application.Handlers.Phonenumbers.Commands.Create;
+using Application.Handlers.Phonenumbers.Commands.Delete;
+using Application.Handlers.Phonenumbers.Commands.Update;
+using Application.Handlers.Phonenumbers.Dtos.Commands;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Controllers.Base;
+
+namespace WebApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class PhoneNumbersController : BaseController {
+    [HttpPost("[action]")]
+    [Produces("application/json")]
+    public async Task<IActionResult> Create([FromBody] CreatePhoneNumberCommand createPhoneNumberCommand) {
+        CreatedPhoneNumberDto result = await Mediator.Send(createPhoneNumberCommand);
+        return Created("", result);
+    }
+
+    [HttpDelete("[action]")]
+    [Produces("application/json")]
+    public async Task<IActionResult> Delete([FromBody] DeletePhoneNumberCommand deletePhoneNumberCommand) {
+        DeletedPhoneNumberDto result = await Mediator.Send(deletePhoneNumberCommand);
+        return Ok(result);
+    }
+
+    [HttpPut("[action]")]
+    [Produces("application/json")]
+    public async Task<IActionResult> Update([FromBody] UpdatePhoneNumberCommand updatePhoneNumberCommand) {
+        UpdatedPhoneNumberDto result = await Mediator.Send(updatePhoneNumberCommand);
+        return Ok(result);
+    }
+}
