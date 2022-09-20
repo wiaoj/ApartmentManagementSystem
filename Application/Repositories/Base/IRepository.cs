454cb0f445d9e9e4c5235a0cd3205ae5e0c72850
wiaoj <bertandeniz7@gmail.com>
2022-09-20 21:41:59 +0300

    Repository base updated

--- Application/Repositories/Base/IRepository.cs
+++ Application/Repositories/Base/IRepository.cs
@@ -7,17 +7,17 @@ namespace Application.Repositories.Base;
 public interface IRepository<TypeEntity> where TypeEntity : IBaseEntity, new() {
     DbSet<TypeEntity> Table { get; }
 
-    IQueryable<TypeEntity> GetAll(Boolean tracking);
+    IQueryable<TypeEntity> GetAll(Boolean enableTracking);
 
-    Task<IQueryable<TypeEntity>> GetWhereAsync(Expression<Func<TypeEntity, Boolean>> expression, Boolean tracking);
-    Task<TypeEntity> GetSingleAsync(Expression<Func<TypeEntity, Boolean>> expression, Boolean tracking);
-    Task<TypeEntity> GetByIdAsync(Guid id, Boolean tracking);
+    Task<IQueryable<TypeEntity>> GetWhereAsync(Expression<Func<TypeEntity, Boolean>> expression, Boolean enableTracking);
+    Task<TypeEntity?> GetSingleAsync(Expression<Func<TypeEntity, Boolean>> expression, Boolean enableTracking);
+    Task<TypeEntity?> GetByIdAsync(Guid id, Boolean enableTracking);
 
 
-    Task<Boolean> AddAsync(TypeEntity entity);
-    Task<Boolean> AddRangeAsync(ICollection<TypeEntity> entities);
-    Task<Boolean> DeleteAsync(TypeEntity entity);
-    Task<Boolean> DeleteRangeAsync(ICollection<TypeEntity> entities);
-    Task<Boolean> DeleteAsync(Guid id);
-    Task<Boolean> UpdateAsync(TypeEntity entity);
+    Task AddAsync(TypeEntity entity);
+    Task AddRangeAsync(ICollection<TypeEntity> entities);
+    Task DeleteAsync(TypeEntity entity);
+    Task DeleteRangeAsync(ICollection<TypeEntity> entities);
+    Task DeleteAsync(Guid id);
+    Task UpdateAsync(TypeEntity entity);
 }
--- Persistence/Repositories/Base/Repository.cs
+++ Persistence/Repositories/Base/Repository.cs
@@ -11,14 +11,68 @@ public class Repository<TypeEntity> : IRepository<TypeEntity> where TypeEntity :
 
     public DbSet<TypeEntity> Table => _context.Set<TypeEntity>();
 
-    public Task<Boolean> AddAsync(TypeEntity entity) => throw new NotImplementedException();
-    public Task<Boolean> AddRangeAsync(ICollection<TypeEntity> entities) => throw new NotImplementedException();
-    public Task<Boolean> DeleteAsync(TypeEntity entity) => throw new NotImplementedException();
-    public Task<Boolean> DeleteAsync(Guid id) => throw new NotImplementedException();
-    public Task<Boolean> DeleteRangeAsync(ICollection<TypeEntity> entities) => throw new NotImplementedException();
-    public IQueryable<TypeEntity> GetAll(Boolean tracking) => throw new NotImplementedException();
-    public Task<TypeEntity> GetByIdAsync(Guid id, Boolean tracking) => throw new NotImplementedException();
-    public Task<TypeEntity> GetSingleAsync(Expression<Func<TypeEntity, Boolean>> expression, Boolean tracking) => throw new NotImplementedException();
-    public Task<IQueryable<TypeEntity>> GetWhereAsync(Expression<Func<TypeEntity, Boolean>> expression, Boolean tracking) => throw new NotImplementedException();
-    public Task<Boolean> UpdateAsync(TypeEntity entity) => throw new NotImplementedException();
+    private async Task SaveChangesAsync() => await _context.SaveChangesAsync();
+    public async Task AddAsync(TypeEntity entity) {
+        await Table.AddAsync(entity);
+        await SaveChangesAsync();
+    }
+
+    public async Task AddRangeAsync(ICollection<TypeEntity> entities) {
+        await Table.AddRangeAsync(entities);
+        await SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(TypeEntity entity) {
+        await Task.Run(async () => {
+            Table.Remove(entity);
+            await SaveChangesAsync();
+        });
+    }
+
+    public async Task DeleteAsync(Guid id) {
+        await Task.Run(async () => {
+            TypeEntity? entity = await GetByIdAsync(id, enableTracking: true);
+            Table.Remove(entity);
+            await SaveChangesAsync();
+        });
+    }
+
+    public async Task DeleteRangeAsync(ICollection<TypeEntity> entities) {
+        await Task.Run(async () => {
+            Table.RemoveRange(entities);
+            await SaveChangesAsync();
+        });
+    }
+
+    public IQueryable<TypeEntity> GetAll(Boolean enableTracking) {
+        IQueryable<TypeEntity> query = Table.AsQueryable();
+        return enableTracking ? query : query.AsNoTracking();
+    }
+
+    public async Task<TypeEntity?> GetByIdAsync(Guid id, Boolean enableTracking) {
+        return enableTracking ? await Table.FindAsync(id) : await Table.FirstOrDefaultAsync(x => x.Id.Equals(id));
+    }
+
+    public async Task<TypeEntity?> GetSingleAsync(Expression<Func<TypeEntity, Boolean>> expression, Boolean enableTracking) {
+        return await Task.Run(() => {
+            IQueryable<TypeEntity> query = Table.AsQueryable();
+            return enableTracking ?
+                query.SingleOrDefaultAsync(expression) :
+                query.AsNoTracking().SingleOrDefaultAsync(expression);
+        });
+    }
+
+    public async Task<IQueryable<TypeEntity>> GetWhereAsync(Expression<Func<TypeEntity, Boolean>> expression, Boolean enableTracking) {
+        return await Task.Run(() => {
+            IQueryable<TypeEntity> query = Table.Where(expression);
+            return enableTracking ? query : query.AsNoTracking();
+        });
+    }
+
+    public async Task UpdateAsync(TypeEntity entity) {
+        await Task.Run(async () => {
+            Table.Update(entity);
+            await SaveChangesAsync();
+        });
+    }
 }
